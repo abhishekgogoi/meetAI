b7385f74411ebfbb82822aa1086b7a884f4161e9
Abhishek Gogoi <abhisat495@gmail.com>
2025-09-21 13:35:22 +0530

    07:dashboard-navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import DashboardSidebar from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import DashboardNavbar from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { ReactNode } from "react";
 
 
@@ -11,7 +12,10 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <SidebarProvider>
       <DashboardSidebar />
-      <main className="flex flex-col h-screen w-screen bg-muted">{children}</main>
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
+        {children}
+      </main>
     </SidebarProvider>
   );
 };
